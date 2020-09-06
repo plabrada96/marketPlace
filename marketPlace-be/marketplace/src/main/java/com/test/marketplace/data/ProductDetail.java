00c271b3e236d686dbefe3075988a031031a11c2
Paola Andrea Labrada España <plabrada82@gmail.com>
2020-09-05 20:08:52 -0500

    init front

---
+++ marketPlace-be/marketplace/src/main/java/com/test/marketplace/data/ProductDetail.java
@@ -0,0 +1,105 @@
+package com.test.marketplace.data;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+import com.test.marketplace.common.data.GenericEntity;
+import com.test.marketplace.constants.UtilConstants;
+
+@Entity
+@Table(schema=UtilConstants.SCHEMA_DB, name ="product_detail")
+public class ProductDetail extends GenericEntity {
+
+	private static final long serialVersionUID = -7321372148343663458L;
+	
+	@ManyToOne(fetch=FetchType.LAZY)
+	@JoinColumn(name = "id_product")
+	private Product product;
+	
+	@Column(name = "color")
+	private String color;
+	
+	@Column(name = "brand")
+	private String brand;
+	
+	@Column(name = "mark")
+	private String mark;
+	
+	@Column(name = "price")
+	private Integer price;
+	
+	@Column(name = "stock")
+	private Integer stock;
+	
+	@Column(name = "description", columnDefinition = "TEXT")
+	private String description;
+	
+	public ProductDetail() {
+		super();
+	}
+	
+	
+	public String getBrand() {
+		return brand;
+	}
+
+
+
+	public void setBrand(String brand) {
+		this.brand = brand;
+	}
+
+
+
+	public String getMark() {
+		return mark;
+	}
+
+
+	public void setMark(String mark) {
+		this.mark = mark;
+	}
+
+
+	public Product getProduct() {
+		return product;
+	}
+
+
+	public void setProduct(Product product) {
+		this.product = product;
+	}
+
+
+	public String getColor() {
+		return color;
+	}
+	public void setColor(String color) {
+		this.color = color;
+	}
+	
+	public Integer getPrice() {
+		return price;
+	}
+
+	public void setPrice(Integer price) {
+		this.price = price;
+	}
+
+	public Integer getStock() {
+		return stock;
+	}
+	public void setStock(Integer stock) {
+		this.stock = stock;
+	}
+	public String getDescription() {
+		return description;
+	}
+	public void setDescription(String description) {
+		this.description = description;
+	}
+}
---
+++ marketPlace-be/marketplace/src/main/java/com/test/marketplace/webcontroller/ProductController.java
@@ -0,0 +1,53 @@
+package com.test.marketplace.webcontroller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.test.marketplace.common.webcontroller.CommonController;
+import com.test.marketplace.constants.MappingConstants;
+import com.test.marketplace.constants.SwaggerConstants;
+import com.test.marketplace.data.Product;
+import com.test.marketplace.service.ProductService;
+
+import io.swagger.annotations.Api;
+
+@RestController
+@Api(value = SwaggerConstants.API_VALUE_PRODUCT, tags = { SwaggerConstants.API_TAGS_PRODUCT })
+@RequestMapping(SwaggerConstants.API_VALUE_PRODUCT)
+public class ProductController extends CommonController<Product>{
+
+	@Autowired
+	private ProductService service;
+	
+	@Override
+	public Product consult(@PathVariable(MappingConstants.PATH_ID) Long id) {
+		return service.consult(id);
+	}
+
+	@Override
+	public Product save(@RequestBody(required = true) Product product) {
+		return service.save(product);
+	}
+
+	@Override
+	public Product update(@PathVariable(MappingConstants.PATH_ID) Long id, @RequestBody(required = true) Product product) {
+		product.setId(id);
+		return service.save(product);
+	}
+
+	@Override
+	public void delete(@PathVariable(MappingConstants.PATH_ID) Long id) {
+		service.delete(id);
+	}
+	
+	@GetMapping("")
+	public List<Product> list() {
+		return service.list();
+	}
+}
